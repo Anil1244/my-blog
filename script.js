9d04d0539c446f56af5168a76ad6d16ce23954d2
Anil Paudel <gaindakot.anil@gmail.com>
2024-12-02 20:49:33 +0545

    Update site for improved user experience

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the form from submitting
-    alert('Thanks for contacting me!');
+    event.preventDefault();
+    alert('Thanks for your message!');
 });
