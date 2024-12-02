cf2f640a9d10a13a7ee4b7bf4f4f77634307ba76
Anil Paudel <gaindakot.anil@gmail.com>
2024-12-02 20:00:31 +0545

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the form from submitting
+    alert('Thanks for contacting me!');
+});
